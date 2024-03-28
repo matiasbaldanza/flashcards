6727c19df4eb2abe69ae6023fe55a418fdc18d61
Matias Baldanza <matiasbaldanza@gmail.com>
2024-03-28 09:47:46 -0300

    fix(client): make DeckCard actions optional

--- client/src/components/DeckCard.tsx
+++ client/src/components/DeckCard.tsx
@@ -3,7 +3,7 @@ export type DeckType = {
   _id: string,
   title: string,
   description: string,
-  actions: {
+  actions?: {
     delete: (_id: string) => void,
   },
 };
@@ -30,7 +30,7 @@ export default function DeckCard({
       {/* Actions */}
       <button
         className='opacity-0 absolute top-1 right-1 bg-red-500 aspect-square text-white px-2 py-1 rounded-md group-hover:opacity-90 transition-opacity duration-300'
-        onClick={() => actions.delete(_id)}
+        onClick={() => actions?.delete(_id)}
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
           <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
