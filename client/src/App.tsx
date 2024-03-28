ebe1ef6ef698102acdd60a47d1de006415fd4e85
Matias Baldanza <matiasbaldanza@gmail.com>
2024-03-27 23:33:09 -0300

    feat(client): list decks, extract DeckCard component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const EMPTY_DECK = {
+import DeckCard, { DeckType } from './components/DeckCard';
+
+const EMPTY_DECK: DeckType = {
+  _id: '',
   title: '',
   description: '',
 };
 
 function App() {
-  const [deckData, setDeckData] = useState(EMPTY_DECK);
+  const [deckData, setDeckData] = useState<DeckType>(EMPTY_DECK);
+  const [decks, setDecks] = useState<DeckType[]>([]);
 
   async function handleCreateDeck(e: React.FormEvent) {
     e.preventDefault();
@@ -19,70 +23,90 @@ function App() {
         },
         body: JSON.stringify(deckData),
       });
-      const data = await res.json();
-      console.log(data);
+      console.log('Posted:', res);
       setDeckData(EMPTY_DECK);
     } catch (err) {
       console.error(err);
     }
   }
 
-  return (
-    <div>
-      <h1 className='text-2xl font-bold text-black text-left'>
-        Flashcards App
-      </h1>
+  useEffect(() => {
+    async function fetchDecks() {
+      const response = await fetch('http://localhost:5174/decks/');
+      const decks = await response.json();
+      setDecks(decks);
+    }
+    fetchDecks();
+  }, []);
 
-      <form
-        className='flex flex-col gap-4 items-start my-4 py-4'
-        onSubmit={handleCreateDeck}
-      >
+  return (
+    <div className='container mx-auto w-[80%] px-4'>
+      <header>
+        <h1 className='text-2xl font-bold text-black text-left'>
+          Flashcards App
+        </h1>
+      </header>
+      <main>
         <div
-          className='flex flex-col gap-1 items-start'
+          className='grid grid-cols-3 gap-2 items-start mx-auto my-4'
         >
-          <label
-            className='mr-2 font-medium block'
-            htmlFor="deck-title"
-          >
-            Deck Title
-          </label>
-          <input
-            className='border border-gray-400 px-4 py-2 rounded-md'
-            type="text"
-            id="deck-title"
-            name="deck-title"
-            placeholder="Deck Title"
-            value={deckData.title}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setDeckData({ ...deckData, title: e.target.value });
-            }}
-          />
+          {
+            decks.map((deck: DeckType) => <DeckCard {...deck} key={deck._id} />)
+          }
         </div>
-        <div
-          className='flex flex-col gap-1 items-start'
-        >
-          <label
-            className='mr-2 font-medium block'
-            htmlFor="deck-title"
+        <div>
+          <form
+            className='flex flex-col gap-4 items-start my-4 py-4'
+            onSubmit={handleCreateDeck}
           >
-            Description
-          </label>
-          <input
-            className='border border-gray-400 px-4 py-2 rounded-md'
-            type="text"
-            id="deck-title"
-            name="deck-title"
-            placeholder="Description"
-            value={deckData.description}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setDeckData({ ...deckData, description: e.target.value });
-            }}
-          />
+            <div
+              className='flex flex-col gap-1 items-start'
+            >
+              <label
+                className='mr-2 font-medium block'
+                htmlFor="deck-title"
+              >
+                Deck Title
+              </label>
+              <input
+                className='border border-gray-400 px-4 py-2 rounded-md'
+                type="text"
+                id="deck-title"
+                name="deck-title"
+                placeholder="Deck Title"
+                value={deckData.title}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setDeckData({ ...deckData, title: e.target.value });
+                }}
+              />
+            </div>
+            <div
+              className='flex flex-col gap-1 items-start'
+            >
+              <label
+                className='mr-2 font-medium block'
+                htmlFor="deck-title"
+              >
+                Description
+              </label>
+              <input
+                className='border border-gray-400 px-4 py-2 rounded-md'
+                type="text"
+                id="deck-title"
+                name="deck-title"
+                placeholder="Description"
+                value={deckData.description}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setDeckData({ ...deckData, description: e.target.value });
+                }}
+              />
+            </div>
+            <button
+              className='bg-blue-500 text-white px-4 py-2 rounded-md mt-4'
+            >Create Deck</button>
+          </form>
         </div>
-        <button
-          className='bg-blue-500 text-white px-4 py-2 rounded-md mt-4'
-        >Create Deck</button>
-      </form>
+      </main>
     </div>
   )
 }
---
+++ client/src/components/DeckCard.tsx
@@ -0,0 +1,20 @@
+
+export type DeckType = {
+  _id: string,
+  title: string,
+  description: string,
+};
+
+export default function DeckCard({
+  _id,
+  title,
+  description,
+}: DeckType) {
+  return (<div
+    key={_id}
+    className='border border-gray-400 rounded-md p-4 aspect-video'
+  >
+    <h2 className='text-xl font-bold'>{title}</h2>
+    <p>{description}</p>
+  </div>)
+}
